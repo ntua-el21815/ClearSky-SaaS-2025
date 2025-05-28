b2b250bf2a6ff6429f80efbea9b6c3d67e80080e
ntua-el21815 <el21815@mail.ntua.gr>
2025-05-28 21:40:13 +0300

    Change rabbitmq example

--- rabbit_mq/examples/simple_publisher.py
+++ rabbit_mq/examples/simple_publisher.py
@@ -15,9 +15,43 @@ def main():
     conn = pika.BlockingConnection(params)
     ch = conn.channel()
 
+    payload_req = {
+        "type" : "request",
+        "data" : {
+            "id": 123,
+            "content": "This is a test message"
+        }
+    }
+
+    payload_rep = {
+        "type": "reply",
+        "data": {
+            "id": 123,
+            "status": "processed"
+        }
+    }
+
+    payload_stats = {
+        "type": "statistics",
+        "data": {
+            "total_requests": 100,
+            "successful_requests": 95
+        }
+    }
+
+    payload_notif = {
+        "type": "notification",
+        "data": {
+            "user_id": 456,
+            "message": "You have succesfully registered!"
+        }
+    }
+
+    payloads = [payload_req, payload_rep, payload_stats, payload_notif]
+
     queues = ['review_requests', 'review_replies', 'create_statistics', 'notifications']
-    for q in queues:
-        message = {'queue': q, 'payload': f'Hello {q}!'}
+    for i, q in enumerate(queues):
+        message = payloads[i]     
         ch.basic_publish(exchange='',
                          routing_key=q,
                          body=json.dumps(message),
